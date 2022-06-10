27d47c1897c384aa1f6688208b5f66680e9a1e15
Safayet <mrishafayet003@gmail.com>
2022-06-10 15:26:08 +0600

    simple firebase auth sloved

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,6 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  /* apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID */
   apiKey: "AIzaSyB769wnZSOnnNAFPAX0PRJNx9tW9q1zbRM",
   authDomain: "doctors-portal-64105.firebaseapp.com",
   projectId: "doctors-portal-64105",
