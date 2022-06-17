37d564c557d2493042657f0d832dad570c50b2ff
Safayet <mrishafayet003@gmail.com>
2022-06-17 22:02:56 +0600

    api hosting link added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -9,10 +9,10 @@ const AvailableAppointments = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     const formattedDate = format(date, 'PP');
-    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    const { data: services, isLoading, refetch } = useQuery(['available', formattedDate], () => fetch(`https://secure-taiga-44770.herokuapp.com/available?date=${formattedDate}`)
         .then(res => res.json()))
 
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
 
@@ -32,7 +32,7 @@ const AvailableAppointments = ({ date }) => {
                 date={date}
                 treatment={treatment}
                 setTreatment={setTreatment}
-                refetch = {refetch}
+                refetch={refetch}
             ></BookingModal>}
         </div>
     );
